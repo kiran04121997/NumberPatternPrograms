cbc6c0caa24c55890564dd35226d1c1e0826711a
kiran04121997 <bhosalekiran259@gmail.com>
2022-05-20 11:32:10 +0530

    [kiran_bhosale]pattern

---
+++ NumberPatternCode/NumberPatternCode/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberPatternCode
+{
+    class NumberPattern
+    {
+        public static void Main()
+        {
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);     // for similar numbers to each row add "i" instead of j;
+                }
+                Console.WriteLine();
+            }
+
+            /*int count = 0;       // for traingular numbers
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    count++;
+                    Console.Write(count);    
+                }
+                Console.WriteLine();
+            }*/
+            Console.ReadLine();
+        }
+    }
+}
